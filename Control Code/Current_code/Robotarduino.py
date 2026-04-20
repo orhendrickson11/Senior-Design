09f8a87c7ec3b18424c5ec68fc0e8754c43ed4ce
Xinran <xiy225@pitt.edu>
2026-04-20 12:04:18 -0400

    Final Version

--- Control Code/Current_code/Robotarduino.py
+++ Control Code/Current_code/Robotarduino.py
@@ -5,7 +5,6 @@ from standardbots import models, StandardBotsRobot
 
 # ================= Robot Connection =================
 # Set up robot connection over ethernet
-# Set up robot connection over ethernet
 sdk = StandardBotsRobot(
     url='http://192.168.110.5:3000',
     # ⬆ used at lab
@@ -29,35 +28,38 @@ def write_read(x):
     response = arduino.readline().decode().strip()
     return response
 
-def speak_to_arduino():
+def speak_to_arduino1():
     time.sleep(2) # Wait for Arduino to initialize after connection
     print("Connected to Arduino")
     # Send '1' once
     num = "1" 
     value = write_read(num)
+   # print(f"Arduino responded: {value}")
+
+    #arduino.close() # Close the serial connection
+
+def speak_to_arduino2():
+    time.sleep(2) # Wait for Arduino to initialize after connection
+    print("Connected to Arduino")
+    # Send '1' once
+    num = "2" 
+    value = write_read(num)
     print(f"Arduino responded: {value}")
 
-    arduino.close() # Close the serial connection
+    #arduino.close() # Close the serial connection
+
 
 # ================= Global States =================
-ORIENTATION_SNAPSHOT =  models.Quaternion(1.0, 0.0, -1.0, 0.0)
-
-# Current orientation (used for true task-space control)
+# Current orientation 
 CURRENT_ORIENTATION = models.Quaternion(1.0, 0.0, -1.0, 0.0)
-
-# spin control 
-CURRENT_SPIN_DEG = 0.0
 
 
 # ================= Quaternion Utilities =================
 def quaternion_multiply(q1, q2):
-    """
+    '''
     Multiply two quaternions (q1 ⊗ q2)
     Used for LOCAL rotation (tool frame)
-    
-    --- why using local rotation? ---
-    
-    """
+    '''
     return models.Quaternion(
         x=q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y,
         y=q1.w*q2.y - q1.x*q2.z + q1.y*q2.w + q1.z*q2.x,
@@ -67,7 +69,15 @@ def quaternion_multiply(q1, q2):
 
 
 def quaternion_from_axis_angle(axis, angle_rad):
-    """
+    s = math.sin(angle_rad / 2.0)
+    return models.Quaternion(
+        x=axis[0] * s,
+        y=axis[1] * s,
+        z=axis[2] * s,
+        w=math.cos(angle_rad / 2.0)
+    )
+
+    '''
     Create quaternion from axis-angle (axis must be normalized)
     Axis is a unit vector
 
@@ -83,15 +93,7 @@ def quaternion_from_axis_angle(axis, angle_rad):
     Output (Quaternions):
         q = (0, 0, 0.707, 0.707)
         
-    """
-    s = math.sin(angle_rad / 2.0)
-    return models.Quaternion(
-        x=axis[0] * s,
-        y=axis[1] * s,
-        z=axis[2] * s,
-        w=math.cos(angle_rad / 2.0)
-    )
-
+    '''
 
 # ================= Oritation Generation (Head Down + Self-Rotation) =================
 '''
@@ -107,33 +109,10 @@ def get_downward_drill_orientation(spin_deg):
     return quaternion_multiply(q_base, q_spin)
 
 
-# ================= Snapshot Function =================
-def begin_segment_snapshot_orientation():
-    """
-    Save current tooltip orientation (task-space only)
-    """
-
-    global ORIENTATION_SNAPSHOT
-    global CURRENT_ORIENTATION
-
-    resp = sdk.movement.position.get_arm_position()
-    arm = getattr(resp, "parsed", None) or getattr(resp, "data", None) or resp
-
-    # ---- Save orientation ----
-    ori = arm.tooltip_position.orientation
-    q = ori.quaternion
-    ORIENTATION_SNAPSHOT = models.Quaternion(x=q.x, y=q.y, z=q.z, w=q.w)
-
-    # Sync current orientation
-    CURRENT_ORIENTATION = models.Quaternion(x=q.x, y=q.y, z=q.z, w=q.w)
-
-    # print("[Snapshot] Orientation captured")
-
-
-# ================= Move Function (IK only) =================
+# ================= Move Function=================
 def move_tooltip_xyz(x_m, y_m, z_m):
     """
-    Move tooltip using IK (position + fixed orientation)
+     Move tooltip using IK (position + fixed orientation)
     """
 
     global CURRENT_SPIN_DEG
@@ -158,6 +137,61 @@ def move_tooltip_xyz(x_m, y_m, z_m):
 
     sdk.movement.position.set_arm_position(body=body).ok()
 
+# This function let the tooltip move to a traget point with a defined rotation angle
+def move_with_agnle(target_point, spin_deg):
+    """
+    target_point: (x, y, z_work)
+    safe_point:   (x, y, z_safe)  
+    """
+
+    global CURRENT_SPIN_DEG
+
+    target_x, target_y, target_z = target_point
+
+    # --- Step 1: set orientation ---
+    CURRENT_SPIN_DEG = spin_deg
+    # print(f"[WEAVE] Spin = {spin_deg}")
+
+    # --- Step 2: move to target ---
+    # print(f"[WEAVE] Move to target ({target_x}, {target_y}, {target_z})")
+    move_tooltip_xyz(target_x, target_y, target_z)
+    
+
+# ---- Testing New orientation ----
+# This two functions are used to cover more orientations (Are not used in our current process)
+# Only need to adjust the unit vector below:
+# q_base = quaternion_from_axis_angle((0, 1, 0), math.radians(90))
+'''
+def move_tooltip(x_m, y_m, z_m):
+    
+    global CURRENT_SPIN_DEG
+
+    body = models.ArmPositionUpdateRequest(
+        kind=models.ArmPositionUpdateRequestKindEnum.TooltipPosition,
+        tooltip_position=models.PositionAndOrientation(
+            position=models.Position(
+                x=float(x_m),
+                y=float(y_m),
+                z=float(z_m),
+                unit_kind=models.LinearUnitKind.Millimeters,
+            ),
+            orientation=models.Orientation(
+                kind=models.OrientationKindEnum.Quaternion,
+                quaternion=get_orientation(CURRENT_SPIN_DEG), # NEW UPDATES HERE
+            ),
+        ),
+    )
+    q = get_downward_drill_orientation(CURRENT_SPIN_DEG)
+    print("spin =", CURRENT_SPIN_DEG, "-> q =", q)
+
+    sdk.movement.position.set_arm_position(body=body).ok()
+
+def get_orientation(spin_deg):
+    q_base = quaternion_from_axis_angle((0, 1, 0), math.radians(90))
+    q_spin = quaternion_from_axis_angle((1, 0, 0), math.radians(90))
+    return quaternion_multiply(q_base, q_spin)
+'''
+
 
 # ================= Nail Motion =================
 def wound_nail(nail_point, dx=30, dy=30, delta_deg=0):
@@ -165,15 +199,35 @@ def wound_nail(nail_point, dx=30, dy=30, delta_deg=0):
     Execute cross pattern around a nail, then rotate joint6
     """
 
-    # ---- Step 1: Snapshot ----
-    begin_segment_snapshot_orientation()
+    x, y, z = nail_point
+
+    C = (x,      y,      z)
+    R = (x - dx, y,      z)
+    D = (x,      y + dy, z)
+    L = (x + dx, y,      z)
+    U = (x,      y - dy, z)
+
+    # ---- Step 2: IK Motion ----
+    move_tooltip_xyz(*U)
+    move_tooltip_xyz(*L)
+    move_tooltip_xyz(*D)
+    move_tooltip_xyz(*R)
+    move_tooltip_xyz(*U)
+    move_tooltip_xyz(*L)
+    move_tooltip_xyz(*D)
+
+# Same as wound_nail(), but this function only loop once
+def wound_nail_less_loop(nail_point, dx=30, dy=30, delta_deg=0):
+    """
+    Execute cross pattern around a nail, then rotate joint6
+    """
 
     x, y, z = nail_point
 
     C = (x,      y,      z)
-    L = (x - dx, y,      z)
+    R = (x - dx, y,      z)
     D = (x,      y + dy, z)
-    R = (x + dx, y,      z)
+    L = (x + dx, y,      z)
     U = (x,      y - dy, z)
 
     # ---- Step 2: IK Motion (joint6 allowed to drift) ----
@@ -182,12 +236,8 @@ def wound_nail(nail_point, dx=30, dy=30, delta_deg=0):
     move_tooltip_xyz(*D)
     move_tooltip_xyz(*R)
     move_tooltip_xyz(*U)
-    move_tooltip_xyz(*L)
-    move_tooltip_xyz(*D)
-    move_tooltip_xyz(*R)
-    move_tooltip_xyz(*U)
-    move_tooltip_xyz(*L)
-    move_tooltip_xyz(*D)
+
+
 
 
 
@@ -216,7 +266,7 @@ def weave(target_point, safe_point, spin_deg):
     move_tooltip_xyz(target_x, target_y, target_z)
 
     # --- Step 4: Motor Rotation:
-    speak_to_arduino()
+    speak_to_arduino1()
 
     # --- Step 5: move back to safe point ---
     # print(f"[WEAVE] Move to safe ({safe_x}, {safe_y}, {safe_z})\n")
@@ -225,58 +275,60 @@ def weave(target_point, safe_point, spin_deg):
 
 # ================= Main =================
 with sdk.connection():
-    '''
-    # Sample code for weaving
-    move_tooltip_xyz(200, 196, 50)
-    move_tooltip_xyz(200, 396, 100)
-    weave((300, 396, 50), (300, 396, 100), 45)
-    # These three lines are only used for testing
-    '''
-    
-    
-    '''
     # Outer circuit
-    wound_nail((100, 350, -15))# √
-    #Outer loop
-    move_tooltip_xyz(200, 196, -15)
-    move_tooltip_xyz(700, 196, -15)
-    move_tooltip_xyz(700, 540, -20)
-    move_tooltip_xyz(600, 680, -25)
-    move_tooltip_xyz(0, 740, -15)
-    move_tooltip_xyz(0, 340, -15)
+    move_tooltip_xyz(185, 465, 85)
+    wound_nail((185, 465, 85))
+    move_tooltip_xyz(285, 311, 75)
+    move_tooltip_xyz(725, 311, 75)
+    move_tooltip_xyz(735, 635, 85)
+    move_tooltip_xyz(405, 890, 75)
+    move_tooltip_xyz(85, 855, 75)
+    move_tooltip_xyz(85, 455, 65)
+    wound_nail((185, 465, 75))
+    wound_nail_less_loop((330, 465, 80))
     
-    wound_nail((100, 350, -15))# √
-    wound_nail((245, 350, -15))# √
-        
     #Inner loop
-    move_tooltip_xyz(480, 270, -15)
-    move_tooltip_xyz(480, 570, -25)
-    move_tooltip_xyz(200, 690, -25)
-    move_tooltip_xyz(120, 540, -25)
+    move_tooltip_xyz(565, 385, 85)
+    move_tooltip_xyz(565, 685, 75)
+    move_tooltip_xyz(285, 785, 74)
+    move_tooltip_xyz(205, 655, 77)
+    move_tooltip_xyz(205, 445, 82)
+    wound_nail_less_loop((330, 465, 85))
     
-    move_tooltip_xyz(120, 330, -15)
-    move_tooltip_xyz(320, 330, -15)
-    wound_nail((245, 350, -15))# √
-    '''
-
-    # scanfold #1 weaving
-    # weave((295, 365, -45), (295, 365, 5), 205)
-    #weave((415, 295, -45), (415, 295, 5), 180)
-    # weave((395, 415, -45), (395, 415, 5), 150)
-    # weave((515, 415, -45), (515, 415, 5), 115)
-    # weave((410, 515, -45), (410, 515, 5), 90)
-    # weave((410, 665, -45), (410, 665, 5), 60)
-    # weave((310, 590, -45), (310, 590, 5), 20)
-    # weave((260, 690, -60), (260, 690, 5), 0)
-    # weave((240, 570, -60), (240, 570, 5), 330)
-    # weave((100, 470, -60), (100, 470, 5), 290)
-    # weave((190, 460, -50), (190, 460, 5), 260)
-    weave((170, 320, -50), (170, 320, 5), 250)
-    # weave((295, 365, -45), (295, 365, 5), 205)
+    #Weaving
+    weave((380, 486, 55), (380, 486, 105), 205)
+    weave((500, 410, 55), (500, 410, 105), 180)
+    weave((480, 530, 55), (480, 530, 105), 150)
+    weave((606, 500, 55), (606, 500, 105), 115)
+    weave((520, 630, 55), (520, 630, 105), 90)    
+    weave((495, 780, 55), (495, 780, 105), 60)    
+    weave((415, 720, 55), (415, 720, 105), 25)
+    weave((345, 805, 40), (345, 805, 105), 0)
+    weave((325, 685, 40), (325, 685, 105), 330)
+    weave((191, 585, 40), (191, 585, 105), 290)
+    weave((295, 575, 50), (295, 575, 105), 260)
+    weave((265, 450, 30), (265, 450, 105), 240)
+    wound_nail_less_loop((490, 480, 76))
+    
+    #Ending
+    move_with_agnle((380, 480, 205), 0)
+    move_with_agnle((75, 480, 205), 0)
+    move_with_agnle((75, 480, -7), 0)
+    move_with_agnle((60, 710, -7), 0)
+    speak_to_arduino2()
+    move_with_agnle((115, 730, -7), 0)
 
     
-    '''
-    THIS IS ABSOLUTE POSITION --- BE CAREFUL
-    '''
 
-# Central point of the scanfold (290, 480)
+
+
+    
+    
+    
+    
+
+
+
+
+
+
